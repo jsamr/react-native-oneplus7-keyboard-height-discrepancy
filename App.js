b6e4fce61632eebce79cb62e68ef53ea0e229db1
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-11-14 13:11:18 -0300

    upgrade reproduction to RN 0.63.3

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import {
-  View,
-  TextInput,
-  StatusBar,
-  SafeAreaView
-} from 'react-native';
-import { DebugKeyboardAvoidingView } from './DebugKeyboardAvoidingView'
+import {View, TextInput, StatusBar, SafeAreaView} from 'react-native';
+import {DebugKeyboardAvoidingView} from './DebugKeyboardAvoidingView';
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={{ flex: 1 }}>
+      <SafeAreaView style={{flex: 1}}>
         <DebugKeyboardAvoidingView>
-            <TextInput autoFocus />
-            <View style={{ flex: 1 }} />
-            <View style={{ height: 40, backgroundColor: 'black', width: "100%" }} />
+          <TextInput autoFocus />
+          <View style={{flex: 1}} />
+          <View style={{height: 40, backgroundColor: 'black', width: '100%'}} />
         </DebugKeyboardAvoidingView>
       </SafeAreaView>
     </>
--- DebugKeyboardAvoidingView.js
+++ DebugKeyboardAvoidingView.js
@@ -1,127 +1,153 @@
-import React from 'react'
-import { Keyboard, Platform, View, Text } from 'react-native'
-
+import React from 'react';
+import {Keyboard, View, Text, LayoutAnimation, StatusBar} from 'react-native';
 
 function readableKeyboard(keyboardFrame) {
-    if (!keyboardFrame) {
-      return ''
-    }
-    return `H: ${Math.round(keyboardFrame.height)}, Y: ${Math.round(keyboardFrame.screenY)}`
+  if (!keyboardFrame) {
+    return '';
   }
-  
-  function readableView(viewFrame) {
-    if (!viewFrame) {
-      return ''
-    }
-    return `H: ${Math.round(viewFrame.height)}, Y: ${Math.round(viewFrame.y)}`
+  return `H: ${Math.round(keyboardFrame.height)}, Y: ${Math.round(
+    keyboardFrame.screenY,
+  )}`;
+}
+
+function readableView(viewFrame) {
+  if (!viewFrame) {
+    return '';
   }
+  return `H: ${Math.round(viewFrame.height)}, Y: ${Math.round(viewFrame.y)}`;
+}
 
 export class DebugKeyboardAvoidingView extends React.Component {
-    state = { padding: 0 }
-  
-    _subscriptions = []
-    viewRefGet = React.createRef()
-    viewFrame= undefined
-    endKeyboardFrame = undefined
-  
-    onViewLayout = () => {
-      if (!this.viewRefGet.current) {
-        return
-      }
-      this.viewRefGet.current.measureInWindow(this.onViewMeasure)
+  state = {padding: 0};
+
+  _subscriptions = [];
+  viewRefGet = React.createRef();
+  viewFrame = undefined;
+  endKeyboardFrame = undefined;
+
+  onViewLayout = () => {
+    if (!this.viewRefGet.current) {
+      return;
     }
-  
-    onViewMeasure = (x, y, width, height) => {
-      if (height > 0) {
-        this.viewFrame = { x, y, width, height }
-        this.updatePadding()
-      }
+    this.viewRefGet.current.measureInWindow(this.onViewMeasure);
+  };
+
+  onViewMeasure = (x, y, width, height) => {
+    if (height > 0) {
+      this.viewFrame = {x, y, width, height};
+      this.updatePadding();
     }
-  
-    updatePadding() {
-      this.setState({ padding: this.calculatePadding() })
-    }
-  
-    calculatePadding() {
-      if (!this.viewFrame || !this.endKeyboardFrame) {
-        return 0
-      }
-      return this.endKeyboardFrame.height
-    }
-  
-    handleKeyboardEvent(ev) {
-      this.endKeyboardFrame = ev.endCoordinates
-      this.startKeyboardFrame = ev.startCoordinates
-      if (ev.duration) {
-        LayoutAnimation.animateUpdate(ev.duration)
-      }
-      this.updatePadding()
-    }
-  
-    handleKeyboardWillChangeFrameAndroid = (ev) => {
-      if (!ev) {
-        this.endKeyboardFrame = undefined
-        this.updatePadding()
-        return
-      }
-  
-      this.handleKeyboardEvent(ev)
-    }
-  
-    handleKeyboardWillChangeFrameIOS = (ev) => {
-      this.handleKeyboardEvent(ev)
-    }
-  
-    componentDidMount() {
-      this._subscriptions = [
-        Keyboard.addListener('keyboardDidShow', this.handleKeyboardWillChangeFrameAndroid),
-        Keyboard.addListener('keyboardDidHide', this.handleKeyboardWillChangeFrameAndroid),
-      ]
-    }
-  
-    componentWillUnmount() {
-      this._subscriptions.forEach(subscription => {
-        subscription.remove()
-      })
-    }
-  
-    renderDebug() {
-      return (
-        <>
-          <View
-            style={{
-              position: 'absolute',
-              backgroundColor: 'red',
-              top: 0,
-              right: 0,
-              width: 40,
-              height: this.state.padding,
-            }}
-          />
-          <View style={{ position: 'absolute', top: 0, right: 40, backgroundColor: 'green', maxWidth: 200 }}>
-            <Text style={{ color: 'white' }}>
-              <Text>SAFE AREA KEYBOARD{'\n'}</Text>
-              <Text>PADDING: {Math.round(this.state.padding)}{'\n'}</Text>
-              <Text>KEYBOARD END: {readableKeyboard(this.endKeyboardFrame)}{'\n'}</Text>
-              <Text>VIEW: {readableView(this.viewFrame)}{'\n'}</Text>
-            </Text>
-          </View>
-        </>
-      )
-    }
-  
-    render() {
-      const { children, ...props } = this.props
-      return (
-        <View
-          {...props}
-          ref={this.viewRefGet}
-          style={[this.props.style, { paddingBottom: this.state.padding, flexGrow: 1 }]}
-          onLayout={this.onViewLayout}
-        >
-          {children}
-          {this.renderDebug()}
-        </View>
-      )
-    }
+  };
+
+  updatePadding() {
+    this.setState({padding: this.calculatePadding()});
   }
+
+  calculatePadding() {
+    if (!this.viewFrame || !this.endKeyboardFrame) {
+      return 0;
+    }
+    return this.endKeyboardFrame.height;
+  }
+
+  handleKeyboardEvent(ev) {
+    this.endKeyboardFrame = ev.endCoordinates;
+    this.startKeyboardFrame = ev.startCoordinates;
+    if (ev.duration) {
+      LayoutAnimation.animateUpdate(ev.duration);
+    }
+    this.updatePadding();
+  }
+
+  handleKeyboardWillChangeFrameAndroid = (ev) => {
+    if (!ev) {
+      this.endKeyboardFrame = undefined;
+      this.updatePadding();
+      return;
+    }
+
+    this.handleKeyboardEvent(ev);
+  };
+
+  handleKeyboardWillChangeFrameIOS = (ev) => {
+    this.handleKeyboardEvent(ev);
+  };
+
+  componentDidMount() {
+    this._subscriptions = [
+      Keyboard.addListener(
+        'keyboardDidShow',
+        this.handleKeyboardWillChangeFrameAndroid,
+      ),
+      Keyboard.addListener(
+        'keyboardDidHide',
+        this.handleKeyboardWillChangeFrameAndroid,
+      ),
+    ];
+    this.updatePadding();
+  }
+
+  componentWillUnmount() {
+    this._subscriptions.forEach((subscription) => {
+      subscription.remove();
+    });
+  }
+
+  renderDebug() {
+    return (
+      <>
+        <View
+          style={{
+            position: 'absolute',
+            backgroundColor: 'red',
+            top: 0,
+            right: 0,
+            width: 40,
+            height: this.state.padding,
+          }}
+        />
+        <View
+          style={{
+            position: 'absolute',
+            top: 0,
+            right: 40,
+            backgroundColor: 'green',
+            maxWidth: 200,
+          }}>
+          <Text style={{color: 'white'}}>
+            <Text>SAFE AREA KEYBOARD{'\n'}</Text>
+            <Text>
+              PADDING: {Math.round(this.state.padding)}
+              {'\n'}
+            </Text>
+            <Text>
+              KEYBOARD END: {readableKeyboard(this.endKeyboardFrame)}
+              {'\n'}
+            </Text>
+            <Text>
+              VIEW: {readableView(this.viewFrame)}
+              {'\n'}
+            </Text>
+          </Text>
+        </View>
+      </>
+    );
+  }
+
+  render() {
+    const {children, ...props} = this.props;
+    return (
+      <View
+        {...props}
+        ref={this.viewRefGet}
+        style={[
+          this.props.style,
+          {paddingBottom: this.state.padding, flexGrow: 1},
+        ]}
+        onLayout={this.onViewLayout}>
+        {children}
+        {this.renderDebug()}
+      </View>
+    );
+  }
+}
